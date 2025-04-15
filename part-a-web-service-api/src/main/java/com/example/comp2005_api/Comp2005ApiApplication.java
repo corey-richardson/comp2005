444554ae50f7048dae3635149781bb4dafcb4475
Corey Richardson <richardsoncorey04@gmail.com>
2025-04-15 12:47:30 +0100

    Created spring app

---
+++ part-a-web-service-api/src/main/java/com/example/comp2005_api/Comp2005ApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.comp2005_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp2005ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comp2005ApiApplication.class, args);
+	}
+
+}
---
+++ part-a-web-service-api/src/test/java/com/example/comp2005_api/Comp2005ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comp2005_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp2005ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
