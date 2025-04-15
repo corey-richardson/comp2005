f81b1a5d79a340a94dede1e053a3c946c4886f88
Corey Richardson <richardsoncorey04@gmail.com>
2025-04-15 16:18:45 +0100

    `Id` -> `ID` for internal consistency

--- part-a-web-service-api/src/main/java/com/example/comp2005_api/Admission.java
+++ part-a-web-service-api/src/main/java/com/example/comp2005_api/Admission.java
@@ -1,17 +1,20 @@
 package com.example.comp2005_api;
 
+/* External API uses name 'patientID' where 'ID' is *fully* capitalised, therefore the model also
+* needs to use that naming convention - even if it's not preferred. >:(  */
+
 public class Admission
 {
     private int id;
-    private int patientId;
+    private int patientID;
     private String admissionDate;
     private String dischargeDate;
 
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
 
-    public int getPatientId() { return patientId; }
-    public void setPatientId(int patientId) { this.patientId = patientId; }
+    public int getPatientID() { return patientID; }
+    public void setPatientID(int patientID) { this.patientID = patientID; }
 
     public String getAdmissionDate() { return admissionDate; };
     public void setAdmissionDate(String admissionDate) { this.admissionDate = admissionDate; }
