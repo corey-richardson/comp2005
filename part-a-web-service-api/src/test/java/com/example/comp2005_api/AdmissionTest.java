4f2b07f999a9044a569849a110c93769b11dcc15
Corey Richardson <richardsoncorey04@gmail.com>
2025-04-18 14:05:19 +0100

    Added basic tests

--- part-a-web-service-api/src/test/java/com/example/comp2005_api/AdmissionTest.java
+++ part-a-web-service-api/src/test/java/com/example/comp2005_api/AdmissionTest.java
@@ -1,5 +1,6 @@
 package com.example.comp2005_api;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,5 +8,18 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class AdmissionTest
 {
+    @Test
+    void CreateAdmissionTest() {
+        Admission admission = new Admission();
+        admission.setId(1);
+        admission.setPatientID(1);
+        admission.setAdmissionDate("2024-12-06");
+        admission.setDischargeDate("2024-12-08");
 
+        assertNotNull(admission);
+        assertEquals(1, admission.getId());
+        assertEquals(1, admission.getPatientID());
+        assertEquals("2024-12-06", admission.getAdmissionDate());
+        assertEquals("2024-12-08", admission.getDischargeDate());
+    }
 }
--- part-a-web-service-api/src/test/java/com/example/comp2005_api/AllocationTest.java
+++ part-a-web-service-api/src/test/java/com/example/comp2005_api/AllocationTest.java
@@ -1,8 +1,27 @@
 package com.example.comp2005_api;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class AllocationTest
 {
+    @Test
+    void CreateAllocationTest() {
+        Allocation allocation = new Allocation();
+        allocation.setId(1);
+        allocation.setAdmissionId(2);
+        allocation.setEmployeeId(3);
+        allocation.setStartTime("2024-12-06");
+        allocation.setEndTime("2024-12-08");
 
+        assertNotNull(allocation);
+        assertEquals(1, allocation.getId());
+        assertEquals(2, allocation.getAdmissionId());
+        assertEquals(3, allocation.getEmployeeId());
+        assertEquals("2024-12-06", allocation.getStartTime());
+        assertEquals("2024-12-08", allocation.getEndTime());
+    }
 }
