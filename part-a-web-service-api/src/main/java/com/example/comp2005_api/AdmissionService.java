3d0249272beb1219927ae9725237a161c7ccdda0
Corey Richardson <richardsoncorey04@gmail.com>
2025-04-24 14:18:43 +0100

    F3 Failing Implementation

--- part-a-web-service-api/src/main/java/com/example/comp2005_api/AdmissionService.java
+++ part-a-web-service-api/src/main/java/com/example/comp2005_api/AdmissionService.java
@@ -2,6 +2,10 @@ package com.example.comp2005_api;
 
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
 @Service
 public class AdmissionService
 {
@@ -13,8 +17,41 @@ public class AdmissionService
 
     // F3: Identify which month has the most admissions.
     public String getMonthWithMostAdmissions() {
-        // TODO
+        Admission[] admissions = apiHelper.getAllAdmissions();
 
-        return "NOT IMPLEMENTED";
+        Map<String, Integer> admissionsByMonth = new HashMap<>(); // "YYYY-MM"
+
+        for (Admission admission : admissions) {
+            String admissionDateStr = admission.getAdmissionDate();
+            if (admissionDateStr == null) { continue; }
+
+            LocalDateTime admissionDate;
+            try {
+                admissionDate = LocalDateTime.parse(admissionDateStr);
+            } catch (Exception e) {
+                // Failed to parse date, skip to next date
+                continue;
+            }
+
+            // https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM");
+            String yearMonthKey = String.valueOf(LocalDateTime.parse(admissionDateStr, formatter));
+
+            int count = admissionsByMonth.getOrDefault(yearMonthKey, 0);
+            admissionsByMonth.put(yearMonthKey, count+1);
+        }
+
+        int maxCount = -1;
+        String mostCommonMonth = "";
+        // https://stackoverflow.com/questions/46898/how-do-i-efficiently-iterate-over-each-entry-in-a-java-map
+        for (Map.Entry<String, Integer> entry : admissionsByMonth.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                maxCount = entry.getValue();
+                mostCommonMonth = entry.getKey();
+            }
+        }
+
+        if (mostCommonMonth == null) { return ""; }
+        return mostCommonMonth;
     }
 }
