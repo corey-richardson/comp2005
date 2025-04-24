c2dcf04c7a662b46620184f121c0b27846e3694a
Corey Richardson <richardsoncorey04@gmail.com>
2025-04-24 14:19:51 +0100

    `TEST::handlesErrorsGracefully` passes

--- part-a-web-service-api/src/main/java/com/example/comp2005_api/AdmissionService.java
+++ part-a-web-service-api/src/main/java/com/example/comp2005_api/AdmissionService.java
@@ -17,41 +17,45 @@ public class AdmissionService
 
     // F3: Identify which month has the most admissions.
     public String getMonthWithMostAdmissions() {
-        Admission[] admissions = apiHelper.getAllAdmissions();
+        try {
+            Admission[] admissions = apiHelper.getAllAdmissions();
 
-        Map<String, Integer> admissionsByMonth = new HashMap<>(); // "YYYY-MM"
+            Map<String, Integer> admissionsByMonth = new HashMap<>(); // "YYYY-MM"
 
-        for (Admission admission : admissions) {
-            String admissionDateStr = admission.getAdmissionDate();
-            if (admissionDateStr == null) { continue; }
+            for (Admission admission : admissions) {
+                String admissionDateStr = admission.getAdmissionDate();
+                if (admissionDateStr == null) { continue; }
 
-            LocalDateTime admissionDate;
-            try {
-                admissionDate = LocalDateTime.parse(admissionDateStr);
-            } catch (Exception e) {
-                // Failed to parse date, skip to next date
-                continue;
+                LocalDateTime admissionDate;
+                try {
+                    admissionDate = LocalDateTime.parse(admissionDateStr);
+                } catch (Exception e) {
+                    // Failed to parse date, skip to next date
+                    continue;
+                }
+
+                // https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM");
+                String yearMonthKey = String.valueOf(LocalDateTime.parse(admissionDateStr, formatter));
+
+                int count = admissionsByMonth.getOrDefault(yearMonthKey, 0);
+                admissionsByMonth.put(yearMonthKey, count+1);
             }
 
-            // https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM");
-            String yearMonthKey = String.valueOf(LocalDateTime.parse(admissionDateStr, formatter));
-
-            int count = admissionsByMonth.getOrDefault(yearMonthKey, 0);
-            admissionsByMonth.put(yearMonthKey, count+1);
-        }
-
-        int maxCount = -1;
-        String mostCommonMonth = "";
-        // https://stackoverflow.com/questions/46898/how-do-i-efficiently-iterate-over-each-entry-in-a-java-map
-        for (Map.Entry<String, Integer> entry : admissionsByMonth.entrySet()) {
-            if (entry.getValue() > maxCount) {
-                maxCount = entry.getValue();
-                mostCommonMonth = entry.getKey();
+            int maxCount = -1;
+            String mostCommonMonth = "";
+            // https://stackoverflow.com/questions/46898/how-do-i-efficiently-iterate-over-each-entry-in-a-java-map
+            for (Map.Entry<String, Integer> entry : admissionsByMonth.entrySet()) {
+                if (entry.getValue() > maxCount) {
+                    maxCount = entry.getValue();
+                    mostCommonMonth = entry.getKey();
+                }
             }
-        }
 
-        if (mostCommonMonth == null) { return ""; }
-        return mostCommonMonth;
+            if (mostCommonMonth == null) { return "No admissions found."; }
+            return mostCommonMonth;
+        } catch (Exception e) {
+            return "No admissions found.";
+        }
     }
 }
