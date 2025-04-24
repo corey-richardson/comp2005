673ed591eb18eef65d30c700963870b013c4a051
Corey Richardson <richardsoncorey04@gmail.com>
2025-04-24 15:20:04 +0100

    `@Nested` `ApiHelper` test stumps

--- part-a-web-service-api/src/test/java/com/example/comp2005_api/ApiHelperTest.java
+++ part-a-web-service-api/src/test/java/com/example/comp2005_api/ApiHelperTest.java
@@ -1,6 +1,7 @@
 package com.example.comp2005_api;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,71 +24,107 @@ class ApiHelperTest
 
     Patient mockPatient = new Patient();
 
-    private Patient[] mockPatients;
     private Admission[] mockAdmissions;
+    private Allocation[] mockAllocations;
+    private Employee[] mockEmployees;
+    private Patient[] mockPatients;
 
-    @BeforeEach
-    void setUp()
-    {
-        // Set up mock data
-        Patient p1 = new Patient();
-        p1.setId(1);
-        p1.setFirstName("Viv");
-        p1.setLastName("Robinson");
-        p1.setNhsNumber("1113335555");
+    @Nested
+    class AdmissionTests {
 
-        Patient p2 = new Patient();
-        p2.setId(2);
-        p2.setFirstName("Heather");
-        p2.setLastName("Carter");
-        p2.setNhsNumber("2224446666");
+        @BeforeEach
+        void setUp() {
+            Admission a1 = new Admission();
+            a1.setId(1);
+            a1.setPatientID(1);
 
-        mockPatients = new Patient[]{p1, p2};
-        mockPatient = p1;
+            Admission a2 = new Admission();
+            a2.setId(2);
+            a2.setPatientID(2);
 
-        Admission a1 = new Admission();
-        a1.setId(1);
-        a1.setPatientID(1);
+            mockAdmissions = new Admission[]{a1, a2};
+        }
 
-        Admission a2 = new Admission();
-        a2.setId(2);
-        a2.setPatientID(2);
-
-        mockAdmissions = new Admission[]{a1, a2};
+        // TODO
     }
 
-    @Test
-    void getAllPatients_returnsPatients()
-    {
-        // Arrange
-        when(restTemplate.getForObject("https://web.socem.plymouth.ac.uk/COMP2005/api/Patients", Patient[].class))
-                .thenReturn(mockPatients);
-        // Act
-        Patient[] patients = apiHelper.getAllPatients();
-        // Assert
-        assertNotNull(patients);
-        assertEquals(2, patients.length);
-        assertEquals("Viv", patients[0].getFirstName());
-        assertEquals("Heather", patients[1].getFirstName());
+    @Nested
+    class AllocationTests {
+
+        @BeforeEach
+        void setUp() {
+            // TODO
+        }
+
+        // TODO
     }
 
-    @Test
-    void getPatientById_returnsPatient()
-    {
-        when(restTemplate.getForObject("https://web.socem.plymouth.ac.uk/COMP2005/api/Patients/1", Patient.class))
-                .thenReturn(mockPatient);
-        Patient patient = apiHelper.getPatientById(1);
-        assertNotNull(patient);
-        assertEquals(1, patient.getId());
-        assertEquals("Viv", patient.getFirstName());
+    @Nested
+    class EmployeeTests {
+
+        @BeforeEach
+        void setUp() {
+            // TODO
+        }
+
+        // TODO
     }
 
-    @Test
-    void getPatientById_handles404()
-    {
-        when(restTemplate.getForObject("https://web.socem.plymouth.ac.uk/COMP2005/api/Patients/999", Patient.class))
-                .thenThrow(new org.springframework.web.client.HttpClientErrorException(HttpStatus.NOT_FOUND));
-        Patient patient = apiHelper.getPatientById(999);
-        assertNull(patient, "Patient should be null when the ID does not exist.");
+    @Nested
+    class PatientTests {
+
+        @BeforeEach
+        void setUp() {
+            // Set up mock data
+            Patient p1 = new Patient();
+            p1.setId(1);
+            p1.setFirstName("Viv");
+            p1.setLastName("Robinson");
+            p1.setNhsNumber("1113335555");
+
+            Patient p2 = new Patient();
+            p2.setId(2);
+            p2.setFirstName("Heather");
+            p2.setLastName("Carter");
+            p2.setNhsNumber("2224446666");
+
+            mockPatients = new Patient[]{p1, p2};
+            mockPatient = p1;
+        }
+
+        @Test
+        void getAllPatients_returnsPatients()
+        {
+            // Arrange
+            when(restTemplate.getForObject("https://web.socem.plymouth.ac.uk/COMP2005/api/Patients", Patient[].class))
+                    .thenReturn(mockPatients);
+            // Act
+            Patient[] patients = apiHelper.getAllPatients();
+            // Assert
+            assertNotNull(patients);
+            assertEquals(2, patients.length);
+            assertEquals("Viv", patients[0].getFirstName());
+            assertEquals("Heather", patients[1].getFirstName());
+        }
+
+        @Test
+        void getPatientById_returnsPatient()
+        {
+            when(restTemplate.getForObject("https://web.socem.plymouth.ac.uk/COMP2005/api/Patients/1", Patient.class))
+                    .thenReturn(mockPatient);
+            Patient patient = apiHelper.getPatientById(1);
+            assertNotNull(patient);
+            assertEquals(1, patient.getId());
+            assertEquals("Viv", patient.getFirstName());
+        }
+
+        @Test
+        void getPatientById_handles404()
+        {
+            when(restTemplate.getForObject("https://web.socem.plymouth.ac.uk/COMP2005/api/Patients/999", Patient.class))
+                    .thenThrow(new org.springframework.web.client.HttpClientErrorException(HttpStatus.NOT_FOUND));
+            Patient patient = apiHelper.getPatientById(999);
+            assertNull(patient, "Patient should be null when the ID does not exist.");
+        }
     }
 }
